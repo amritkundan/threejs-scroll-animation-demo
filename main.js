b79c5826f1ef90540dc7bd291c3c6cbeab286583
amritkundan <66768804+amritkundan@users.noreply.github.com>
2023-02-17 01:06:15 -0500

    Update main.js

--- main.js
+++ main.js
@@ -73,21 +73,7 @@ scene.add(jeff);
 
 // Moon
 
-const moonTexture = new THREE.TextureLoader().load('moon.jpg');
-const normalTexture = new THREE.TextureLoader().load('normal.jpg');
 
-const moon = new THREE.Mesh(
-  new THREE.SphereGeometry(3, 32, 32),
-  new THREE.MeshStandardMaterial({
-    map: moonTexture,
-    normalMap: normalTexture,
-  })
-);
-
-scene.add(moon);
-
-moon.position.z = 30;
-moon.position.setX(-10);
 
 jeff.position.z = -5;
 jeff.position.x = 2;
