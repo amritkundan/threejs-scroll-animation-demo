fc6a97fc5f29b460e771accdbdfcfc3f9a2b1e57
amritkundan <66768804+amritkundan@users.noreply.github.com>
2023-02-18 23:23:08 -0500

    Update main.js

--- main.js
+++ main.js
@@ -78,10 +78,7 @@ const normalTexture = new THREE.TextureLoader().load('normal.jpg');
 
 const moon = new THREE.Mesh(
   new THREE.SphereGeometry(3, 32, 32),
-  new THREE.MeshStandardMaterial({
-    map: moonTexture,
-    normalMap: normalTexture,
-  })
+  new THREE.MeshStandardMaterial({ color: 0xffffff })
 );
 
 scene.add(moon);
